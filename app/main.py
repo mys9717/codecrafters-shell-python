271ecf91f769e43bfd5755d5944e3a0684786af1
Marcos <santori291@gmail.com>
2026-05-17 22:09:47 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def main():
     sys.stdout.write("$ ")
     pass
 
-command = input()
-print(f"{command}: command not found")
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
