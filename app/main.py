e0b80c15da93e692390adaebec09509cc0b0704e
Marcos <santori291@gmail.com>
2026-05-17 22:55:44 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def main():
     while True:
         sys.stdout.write("$ ")
         pass
-
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
-
 
 if __name__ == "__main__":
     main()
